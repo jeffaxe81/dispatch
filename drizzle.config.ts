23da98f133ea722e98395a7e2b04a6d8c9842f9e
jeffaxe81 <jeffersonmachado81@gmail.com>
2026-09-03 19:12:47 -0300

    chore: include CP-016 schema module

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ if (!connectionString) {
 }
 
 export default defineConfig({
-  schema: "./drizzle/schema.ts",
+  schema: ["./drizzle/schema.ts", "./drizzle/schema.cp016.ts"],
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
