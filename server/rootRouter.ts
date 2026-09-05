11800e342d00f7460d9ef7d7c84f3fd849b00c9d
jeffaxe81 <jeffersonmachado81@gmail.com>
2026-09-04 22:52:10 -0300

    feat: publish work shift reports in root router

--- server/rootRouter.ts
+++ server/rootRouter.ts
@@ -4,6 +4,8 @@ import { dispatchRouterDependencies } from "./dispatchRuntime";
 import { appRouter } from "./routers";
 import { createWorkShiftAdjustmentsRouter } from "./workShiftAdjustmentsRouter";
 import { workShiftAdjustmentsRouterDependencies } from "./workShiftAdjustmentsRuntime";
+import { createWorkShiftReportsRouter } from "./workShiftReportsRouter";
+import { workShiftReportsRouterDependencies } from "./workShiftReportsRuntime";
 import { createWorkShiftSchedulesRouter } from "./workShiftSchedulesRouter";
 import { workShiftSchedulesRouterDependencies } from "./workShiftSchedulesRuntime";
 
@@ -15,6 +17,10 @@ const workShiftAdjustmentsRoot = router({
   workShiftAdjustments: createWorkShiftAdjustmentsRouter(workShiftAdjustmentsRouterDependencies),
 });
 
+const workShiftReportsRoot = router({
+  workShiftReports: createWorkShiftReportsRouter(workShiftReportsRouterDependencies),
+});
+
 const dispatchRoot = router({
   dispatch: createDispatchRouter(dispatchRouterDependencies),
 });
@@ -23,6 +29,7 @@ export const rootRouter = mergeRouters(
   appRouter,
   workShiftSchedulesRoot,
   workShiftAdjustmentsRoot,
+  workShiftReportsRoot,
   dispatchRoot,
 );
 export type RootRouter = typeof rootRouter;
