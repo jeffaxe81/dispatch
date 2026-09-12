6ee2279f0de1445f00d9248374961a0d52c71b46
jeffaxe81 <jeffersonmachado81@gmail.com>
2026-09-12 08:01:55 -0300

    feat: mount tenant boundary shell

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import { trpc } from "@/lib/trpc";
+import { TenantBoundaryShell } from "@/components/TenantBoundaryShell";
 import { UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
@@ -60,7 +61,9 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <TenantBoundaryShell>
+        <App />
+      </TenantBoundaryShell>
     </QueryClientProvider>
   </trpc.Provider>
 );
